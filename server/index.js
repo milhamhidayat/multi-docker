8b997406d6dc29fc0e292d18a85b5f8a302d5583
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-03-22 16:33:25 +0700

    generating react app

--- server/index.js
+++ server/index.js
@@ -24,3 +24,46 @@ pgClient.on('error', () => console.log('Lost PG Connnection'))
 
 pgClient.query("CREATE TABLE IF NOT EXISTS values (number INT)")
     .catch(err => console.log(err))
+
+// redic client setup
+const redis = require('redis')
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+})
+const redisPublisher = redisClient.duplicate()
+
+// express route handlers
+app.get('/', (req, res) => {
+    res.send("hi")
+})
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * from values')
+    res.send(values.rows)
+})
+
+app.get('/values/current', async (req, res) => {
+    redisClient.hgetall9('values', (err, values) => {
+        res.send(values)
+    })
+})
+
+app.post('/values', (req, res) => {
+    const index = req.body.index
+    if (parseInt(index) > 40) {
+        return res.status(422).send('Index too high')
+    }
+
+    redisClient.hset('values', index, 'Nothing yet!')
+    redisPublisher.publish('insert', index)
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index])
+    res.send({
+        working: true
+    })
+})
+
+app.listen(5000, () => {
+    console.log('listening')
+})
