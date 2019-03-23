3fe3000f581479cc4262c23d61b9bfe31c0f4514
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-03-23 17:37:41 +0700

    routing in the react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom
+import OtherPage from './Otherpage'
+import Fib from './Fib'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+              <Link to="/home">Home</Link>
+              <Link to="/otherpage">Other page</Link>
+            </p>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+
     );
   }
 }
