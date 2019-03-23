a8c4d477a3f01041dbdbe822d4a13870f2d72bb5
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-03-23 17:56:11 +0700

    fix link missing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import OtherPage from './Otherpage'
@@ -10,6 +9,10 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
+          <div>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+          </div>
           <div>
             <Route exact path="/" component={Fib} />
             <Route path="/otherpage" component={OtherPage} />
