f9d2430ed3b5d920fdf7bc80098dd33bf28a37b0
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-03-23 17:32:04 +0700

    export fib class

--- client/src/Otherpage.js
+++ client/src/Otherpage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default () => {
     return (
         <div>
-            Im other page
+            I'm other page
             <Link to="/">Go back home</Link>
         </div>
     )
